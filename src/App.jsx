0b358b762dab880963d8896b6eda52c839dadea5
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-10-25 14:59:27 +0500

    simple header logo without styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
-
 function App() {
-
-  return (
-    <div className="App">
-      <p>example</p>
-    </div>
-  )
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <main className="main">
+                    <div className="main__container">
+                        <div className="calculator">
+                            <div className="top">
+                                <h1 className="title">calc</h1>
+                                <div className="theme-changer"></div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+    )
 }
 
 export default App
