87ce37b6c5bfe0c0aca923b90c2b69789aed8305
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-10-28 14:27:44 +0500

    calculator logic

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/prop-types */
-import { Textfit } from "react-textfit";
 import styles from  "./Screen.module.css";
 
 const Screen = ({ value }) => {
   return (
-    <Textfit className={styles.screen} mode="single" max={70}>
+    <div className={styles.screen}>
       {value}
-    </Textfit>
+    </div>
   );
 };
 
