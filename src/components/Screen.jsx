a26c4e0a07bbed2e7268b25f3a0cf6ed9c687115
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-10-29 13:24:19 +0500

    screen theme reapproach

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -4,24 +4,22 @@ import styles from './Screen.module.css'
 const Screen = ({ theme, value }) => {
     return (
         <div
-            style={
+            className={`${styles.headerBottom} ${
                 theme === 'dark'
-                    ? { backgroundColor: 'hsl(224, 36%, 15%)' }
+                    ? styles.darkBg
                     : theme === 'light'
-                    ? { backgroundColor: 'hsl(0, 0%, 93%)' }
-                    : { backgroundColor: 'hsl(268, 71%, 12%)' }
-            }
-            className={styles.headerBottom}
+                    ? styles.lightBg
+                    : styles.blueBg
+            }`}
         >
             <p
-                style={
+                className={`${styles.outputPanel} ${
                     theme === 'dark'
-                        ? { color: 'hsl(0, 0%, 100%)' }
+                        ? styles.textDark
                         : theme === 'light'
-                        ? { color: 'hsl(60, 10%, 19%)' }
-                        : { color: 'hsl(52, 100%, 62%)' }
-                }
-                className={styles.outputPanel}
+                        ? styles.textLight
+                        : styles.textBlue
+                }`}
             >
                 {value}
             </p>
